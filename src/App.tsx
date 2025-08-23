43963117a3db630a0cf256bb869c232cd0cfa5f9
SrayanBhattacharya <bhattacharyasrayan@gmail.com>
2025-08-23 04:46:38 +0000

    Loading screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import LoadingScreen from "./ui/LoadingScreen";
 import MobileMenu from "./ui/MobileMenu";
 import Navbar from "./ui/Navbar";
 import { useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   return (
-    <div>
+    <>
+      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}
       <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-    </div>
+    </>
   );
 }
